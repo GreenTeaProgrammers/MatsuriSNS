08ab63b2867f85048e0c87f7e27d6c3b7d2876ab
Hosi121 <takuyakataiwa@icloud.com>
2024-09-09 00:23:01 +0900

    fix : パスを調整

--- backend/ent/generate.go
+++ backend/ent/generate.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "entgo.io/ent/entc"
-    "entgo.io/ent/entc/gen"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
 )
 
 func main() {
-    err := entc.Generate("./schema", &gen.Config{
-        Target: "./ent",
-    })
-    if err != nil {
-        log.Fatalf("failed to generate ent code: %v", err)
-    }
+	err := entc.Generate("./ent/schema", &gen.Config{
+		Target: "./ent",
+	})
+	if err != nil {
+		log.Fatalf("failed to generate ent code: %v", err)
+	}
 }
-
