02837fd33f2d51d2c831fabbac626fadb54437ac
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-09-13 19:21:43 +0900

    feat: Add home and map pages

---
+++ frontend/src/pages/MapPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MapPage: React.FC = () => {
+  return (
+    <div>
+      <h1>MapPage</h1>
+      <p>これは新しいページです。</p>
+    </div>
+  );
+};
+
+export default MapPage;
