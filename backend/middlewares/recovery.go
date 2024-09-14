5d62f3550dbcebfec2c0466f41f1936a87b053c8
Hosi121 <takuyakataiwa@icloud.com>
2024-09-15 05:36:07 +0900

    feat: recoveryミドルウェアを実装

---
+++ backend/middlewares/recovery.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RecoveryMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Recovered from panic: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			}
+		}()
+		c.Next()
+	}
+}
